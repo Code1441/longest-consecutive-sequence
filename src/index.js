f47d084d09ef55adff8d2b31002b1c1503d1f7e7
Tanya Gelahova <31937370+Code1441@users.noreply.github.com>
2018-02-26 16:19:40 +0300

    fix: problem with similar data

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ module.exports = function longestConsecutiveLength(array) {
 
     var currCount =1; result = [];
     for(var i = 0; i < array.length; i++){
+        if(array[i]==array[i+1]) continue;
         if(array[i]+1==array[i+1]) currCount++;
         else {
             result.push(currCount);
